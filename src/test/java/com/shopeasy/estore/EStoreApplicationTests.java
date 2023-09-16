beb4a2423a3776ce0f210d2b7aba5de6be9ec931
surinder0322@gmail.com <sunny@sunnys-laptop.lan>
2023-09-16 16:05:56 -0400

    first spring project

---
+++ src/test/java/com/shopeasy/estore/EStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopeasy.estore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
