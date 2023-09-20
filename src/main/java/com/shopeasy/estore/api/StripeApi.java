18aa6d913e89be83a1288a4a5b2c30c8af0ee63f
swetaa <swetaabhugaloo@gmail.com>
2023-09-20 19:38:42 -0400

    Stripe Payment

---
+++ src/main/java/com/shopeasy/estore/api/StripeApi.java
@@ -0,0 +1,30 @@
+package com.shopeasy.estore.api;
+
+import com.shopeasy.estore.dto.StripeChargeDto;
+import com.shopeasy.estore.dto.StripeTokenDto;
+import com.shopeasy.estore.service.StripeService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/stripe")
+@AllArgsConstructor
+public class StripeApi {
+
+    private final StripeService stripeService;
+
+   //card info is added, if card info incorrect no token is generated and thus no charge
+    @PostMapping("/card/token")
+    @ResponseBody
+    public StripeTokenDto createCardToken(@RequestBody StripeTokenDto model){
+        return stripeService.createCardToken(model);
+
+    }
+
+    @PostMapping("/charge")
+    @ResponseBody
+    public StripeChargeDto charge(@RequestBody StripeChargeDto model){
+        return stripeService.charge(model);
+    }
+
+}
