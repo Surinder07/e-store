0623e147ce905efd1b8cc4fb643605f1b7e7ead6
chintan ghevariya <73758067+chintanghevariya@users.noreply.github.com>
2023-09-23 14:24:58 -0400

    added add product functionality for product with error DTOs and validation

---
+++ src/main/java/com/shopeasy/estore/dto/ErrorDto.java
@@ -0,0 +1,25 @@
+package com.shopeasy.estore.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
+
+@Builder
+@Data
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDto {
+    //@ApiModelProperty(example = "400")
+    private int statusCode;
+    //@ApiModelProperty(example = "Customer Not Found")
+    private String message;
+    private Instant timestamp;
+    //@ApiModelProperty(example = "Ap123")
+    private String applicationId;
+
+    private String details;
+}
--- src/main/java/com/shopeasy/estore/product/ProductController.java
+++ src/main/java/com/shopeasy/estore/product/ProductController.java
@@ -1,9 +1,14 @@
 package com.shopeasy.estore.product;
 
+import com.shopeasy.estore.dto.ErrorDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -18,9 +23,26 @@ public class ProductController {
     }
 
     @PostMapping("/addProduct")
-    public void addNewProduct(@RequestBody Product product){
+    public ResponseEntity<?> addNewProduct(@RequestBody Product product) {
+        if(product.getProductName().isEmpty() || product.getProductType().isEmpty()
+                || product.getProductDescription().isEmpty()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    ErrorDto.builder().message("ProductName or Type Can't be empty")
+                            .statusCode(HttpStatus.BAD_REQUEST.value())
+                            .timestamp(Instant.now()).build()
+            );
+        }
+        if(product.getPrice() <= 0){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    ErrorDto.builder().message("Product Price Can't be zero or negative ")
+                            .statusCode(HttpStatus.BAD_REQUEST.value())
+                            .timestamp(Instant.now()).build()
+            );
+        }
         productService.saveProduct(product);
+        return ResponseEntity.of(Optional.of(product));
     }
+
 
     @DeleteMapping(path = "deleteProduct/{productId}")
     public void deleteProduct(@PathVariable("productId") Long id){
