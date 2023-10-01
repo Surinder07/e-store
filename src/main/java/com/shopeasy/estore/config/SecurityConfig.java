b57b5a0b62e89eb080b17de6e489c2a8093a1e55
cpratham3 <63926302+cpratham3@users.noreply.github.com>
2023-10-01 00:35:27 -0400

    api docs now working and available at http://localhost:9095/swagger-ui/index.html

--- src/main/java/com/shopeasy/estore/config/SecurityConfig.java
+++ src/main/java/com/shopeasy/estore/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/auth/**","/swagger-ui/**")
+                .requestMatchers("/api/auth/**","/swagger-ui/**","/api-docs/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
