348abfb4b708f81a893b4b06ec2a1074d0b6c679
heerat-singh <heeratsingh54@gmail.com>
2023-10-03 13:23:23 -0400

    updated redis-heerat branch 2

--- src/main/java/com/shopeasy/estore/employee/EmployeeService.java
+++ src/main/java/com/shopeasy/estore/employee/EmployeeService.java
@@ -1,6 +1,8 @@
 package com.shopeasy.estore.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +12,9 @@ public class EmployeeService {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @CachePut(cacheNames = "Employee")
+    @CacheEvict(cacheNames = "Employee",allEntries = true)
     public List<Employee> getEmployeeList(){
 
         return employeeRepository.findAll();
@@ -19,6 +24,7 @@ public class EmployeeService {
         employeeRepository.save(employee);
     }
 
+    @CacheEvict(cacheNames = "Employee",key="#id",allEntries = true)
     public void deleteEmployee(Long id) {
         employeeRepository.deleteById(id);
     }
