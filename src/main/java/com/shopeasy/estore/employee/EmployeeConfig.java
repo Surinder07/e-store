d4de64d7a7f610bd7dcc923822d2e36379271e5d
heerat-singh <heeratsingh54@gmail.com>
2023-10-03 13:09:47 -0400

    updated redis-heerat branch

---
+++ src/main/java/com/shopeasy/estore/employee/EmployeeConfig.java
@@ -0,0 +1,20 @@
+package com.shopeasy.estore.employee;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmployeeConfig {
+   /* @Bean
+    CommandLineRunner commandLineRunner(EmployeeRepository employeeRepository) {
+        return args -> {
+
+            Employee employee1 = new Employee(101L, "surinder", "s@gmail.com", 12, 32);
+            Employee employee2 = new Employee(101L, "suri", "s@gmail.com", 12, 32);
+            Employee employee3 = new Employee(101L, "surinder", "s@gmail.com", 12, 32);
+
+            employeeRepository.saveAll(List.of(employee1, employee2, employee3));
+            }
+        };*/
+
+
+}
