f62f4ebf2fb27b3b05b4710ac0ee8b93d616487a
cpratham3 <63926302+cpratham3@users.noreply.github.com>
2023-10-06 10:31:49 -0400

    added customer entity

---
+++ src/main/java/com/shopeasy/estore/customer/Customer.java
@@ -0,0 +1,25 @@
+package com.shopeasy.estore.customer;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+
+
+}
