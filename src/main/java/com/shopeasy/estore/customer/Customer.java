4abf042d77ab64a6f35e87cb4b3cc4db4714b3c2
cpratham3 <63926302+cpratham3@users.noreply.github.com>
2023-10-06 12:30:16 -0400

    added DTOs for Order

--- src/main/java/com/shopeasy/estore/customer/Customer.java
+++ src/main/java/com/shopeasy/estore/customer/Customer.java
@@ -21,5 +21,8 @@ public class Customer {
     private String name;
     private String email;
 
-
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
 }
--- src/main/java/com/shopeasy/estore/shoppingcart/ShoppingCartController.java
+++ src/main/java/com/shopeasy/estore/shoppingcart/ShoppingCartController.java
@@ -1,4 +1,82 @@
 package com.shopeasy.estore.shoppingcart;
 
+import com.shopeasy.estore.customer.Customer;
+import com.shopeasy.estore.customer.CustomerService;
+import com.shopeasy.estore.dto.OrderDTO;
+import com.shopeasy.estore.dto.ResponseOrderDTO;
+import com.shopeasy.estore.order.*;
+import com.shopeasy.estore.product.Product;
+import com.shopeasy.estore.product.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.time.LocalDateTime;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Random;
+
+@RestController("/api/cart")
 public class ShoppingCartController {
+
+    private final OrderService orderService;
+    private final ProductService productService;
+    private final CustomerService customerService;
+
+
+    public ShoppingCartController(OrderService orderService, ProductService productService, CustomerService customerService) {
+        this.orderService = orderService;
+        this.productService = productService;
+        this.customerService = customerService;
+    }
+
+    private final Logger logger = LoggerFactory.getLogger(ShoppingCartController.class);
+
+    @GetMapping(value = "/getAllProducts")
+    public ResponseEntity<List<Product>> getAllProducts() {
+
+        List<Product> productList = productService.getProductList();
+
+        return ResponseEntity.ok(productList);
+    }
+
+    @GetMapping(value = "/getOrder/{orderId}")
+    public ResponseEntity<Order> getOrderDetails(@PathVariable int orderId) {
+
+        Order order = orderService.getOrderDetail(orderId);
+        return ResponseEntity.ok(order);
+    }
+
+
+    @PostMapping("/placeOrder")
+    public ResponseEntity<ResponseOrderDTO> placeOrder(@RequestBody OrderDTO orderDTO) {
+        logger.info("Request Payload " + orderDTO.toString());
+        ResponseOrderDTO responseOrderDTO = new ResponseOrderDTO();
+        float amount = orderService.getCartAmount(orderDTO.getCartItems());
+
+        Customer customer = new Customer(orderDTO.getCustomerName(), orderDTO.getCustomerEmail());
+        Integer customerIdFromDb = customerService.isCustomerPresent(customer);
+        if (customerIdFromDb != null) {
+            customer.setId(customerIdFromDb);
+            logger.info("Customer already present in db with id : " + customerIdFromDb);
+        }else{
+            customer = customerService.saveCustomer(customer);
+            logger.info("Customer saved.. with id : " + customer.getId());
+        }
+        Order order = new Order(orderDTO.getOrderDescription(), customer, orderDTO.getCartItems());
+        order = orderService.saveOrder(order);
+        logger.info("Order processed successfully..");
+
+        responseOrderDTO.setAmount(amount);
+        responseOrderDTO.setDate(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
+        responseOrderDTO.setInvoiceNumber(new Random().nextInt(1000));
+        responseOrderDTO.setOrderId(order.getId());
+        responseOrderDTO.setOrderDescription(orderDTO.getOrderDescription());
+
+        logger.info("test push..");
+
+        return ResponseEntity.ok(responseOrderDTO);
+    }
+
 }
