73c930029eedb149c7fd920f302a33ebb5857dd9
surinder0322@gmail.com <sunny@Sunnys-Laptop.local>
2023-12-21 20:30:19 -0500

    removed sout

--- src/main/java/com/shopeasy/estore/EStoreApplication.java
+++ src/main/java/com/shopeasy/estore/EStoreApplication.java
@@ -8,7 +8,7 @@ public class EStoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EStoreApplication.class, args);
-        System.out.println("Hello");
+
     }
 
 }
